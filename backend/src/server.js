f79f4a5fdc172991bb7264f2ac1fbad3e1d3c7ac
Vedant Varma <vedantvarma617@gmail.com>
2026-08-01 06:53:06 +0530

    Deployment ready for cheaking changes in deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,28 @@
-import express from "express"; 
-import dotenv from "dotenv"; 
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
 
 import authRoutes from "./routes/auth-route.js";
-import messageRoutes from "./routes/message-route.js"; 
+import messageRoutes from "./routes/message-route.js";
 
-dotenv.config(); 
+dotenv.config();
 
-const app = express(); 
+const app = express();
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 5000;
 
-app.use("/api/auth" , authRoutes); 
-app.use("/api/messages" , messageRoutes); 
-app.listen(PORT, ()=> console.log("Server is running on PORT " + PORT));
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`);
+});
