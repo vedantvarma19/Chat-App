0b3bf04d787b99d5aa2bbc8305d1ddc98d3e5e65
Vedant Varma <vedantvarma617@gmail.com>
2026-08-02 20:11:09 +0530

    Code refactored in signup controller and utils.js file

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => { 
     try {
+    const { MONGO_URI } = process.env;
+    if(!MONGO_URI) throw new Error("MONGO URI is not Set")
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log("MONGO CONNECTED" , conn.connection.host);
    }catch(error){ 
