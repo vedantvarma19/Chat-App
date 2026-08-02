cbedcd3ffc4ea7a502f4d8d6a16fba7215459db3
Vedant Varma <vedantvarma617@gmail.com>
2026-08-02 19:46:18 +0530

    Signup controller added

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,6 +12,9 @@ const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // MiddleWare  ( req.body)
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
